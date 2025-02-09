b825f5fb0ae1b859f4d84c20608d27bed95baf44
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2025-02-09 00:47:29 -0300

    chore: Improve exception

--- src/main/java/com/awakenedredstone/autowhitelist/config/source/AnnotationParserException.java
+++ src/main/java/com/awakenedredstone/autowhitelist/config/source/AnnotationParserException.java
@@ -1,13 +1,7 @@
 package com.awakenedredstone.autowhitelist.config.source;
 
 public class AnnotationParserException extends RuntimeException {
-    private final Throwable original;
-
-    public AnnotationParserException(Throwable original) {
-        this.original = original;
-    }
-
-    public Throwable getOriginal() {
-        return original;
+    public AnnotationParserException(Throwable cause) {
+        super(cause);
     }
 }
