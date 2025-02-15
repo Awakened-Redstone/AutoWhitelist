0cd03b94ca96003cd9dfce1ea0c8cf64bb046f04
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2025-02-15 16:31:49 -0300

    chore: Remove entry from cache on `/whitelist remove`

--- src/main/java/com/awakenedredstone/autowhitelist/mixin/WhitelistCommandMixin.java
+++ src/main/java/com/awakenedredstone/autowhitelist/mixin/WhitelistCommandMixin.java
@@ -1,5 +1,6 @@
 package com.awakenedredstone.autowhitelist.mixin;
 
+import com.awakenedredstone.autowhitelist.AutoWhitelist;
 import com.awakenedredstone.autowhitelist.whitelist.ExtendedWhitelist;
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
@@ -16,6 +17,8 @@ import net.minecraft.server.dedicated.command.WhitelistCommand;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Supplier;
 
@@ -36,6 +39,13 @@ public class WhitelistCommandMixin {
     @WrapWithCondition(method = "executeRemove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/command/ServerCommandSource;sendFeedback(Ljava/util/function/Supplier;Z)V"))
     private static boolean failMessageOnBadEntry(ServerCommandSource source, Supplier<Text> feedbackSupplier, boolean broadcastToOps, @Local Whitelist whitelist, @Local GameProfile gameProfile) {
         return !whitelist.isAllowed(gameProfile);
+    }
+
+    @Inject(method = "executeRemove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/Whitelist;remove(Lnet/minecraft/server/ServerConfigEntry;)V", shift = At.Shift.AFTER))
+    private static void removeWhitelistCache(CallbackInfoReturnable<Integer> cir, @Local Whitelist whitelist, @Local GameProfile gameProfile) {
+        if (!whitelist.isAllowed(gameProfile)) {
+            AutoWhitelist.WHITELIST_CACHE.remove(gameProfile);
+        }
     }
 
     @Definition(id = "i", local = @Local(type = int.class))
