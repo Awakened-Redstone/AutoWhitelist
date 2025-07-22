cc9bce1cbc313f5231f57b1648cdea50d2dca2f7
LunaFox <awakenedredstone@gmail.com>
2025-07-21 21:33:42 -0300

    chore: Fix race condition handling

--- src/main/java/com/awakenedredstone/autowhitelist/discord/api/ReplyCallback.java
+++ src/main/java/com/awakenedredstone/autowhitelist/discord/api/ReplyCallback.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import net.dv8tion.jda.api.utils.messages.MessageEditData;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -16,51 +17,22 @@ public interface ReplyCallback {
     void sendMessage(MessageCreateData message);
     void editMessage(MessageEditData messageData);
 
-    class DefaultInteractionReplyCallback extends InteractionReplyCallback {
-        private final SlashCommandEvent event;
-
-        public DefaultInteractionReplyCallback(SlashCommandEvent event) {
-            this.event = event;
-        }
-
-        public void sendMessage(MessageCreateData messageData) {
-            if (messageData == null) {
-                acknowledgment = acknowledge();
-            } else {
-                acknowledgment = event.reply(messageData).setEphemeral(AutoWhitelist.CONFIG.ephemeralReplies).submit();
-            }
-
-            // Execute the pending task after the acknowledgment
-            acknowledgment.thenAccept(message -> {
-                originalMessage = message;
-                // Only execute if there is a pending task
-                if (pendingTask != null) {
-                    pendingTask.accept(message);
-                }
-            });
-        }
-
-        @Override
-        public CompletableFuture<InteractionHook> acknowledge() {
-            return event.deferReply(AutoWhitelist.CONFIG.ephemeralReplies).submit();
-        }
-    }
-
     abstract class InteractionReplyCallback implements ReplyCallback {
-        protected CompletableFuture<InteractionHook> acknowledgment;
-        public CompletableFuture<Message> executingTask;
-        protected Consumer<InteractionHook> pendingTask;
-        protected InteractionHook originalMessage;
+        protected final AtomicReference<CompletableFuture<InteractionHook>> acknowledgment = new AtomicReference<>();
+        public final AtomicReference<CompletableFuture<Message>> executingTask = new AtomicReference<>();
+        protected final AtomicReference<Consumer<InteractionHook>> pendingTask = new AtomicReference<>();
+        protected final AtomicReference<InteractionHook> originalMessage = new AtomicReference<>();
 
         public void sendMessage(MessageCreateData messageData) {
-            acknowledgment = acknowledge();
+            acknowledgment.set(acknowledge());
 
             // Execute the pending task after the acknowledgment
-            acknowledgment.thenAccept(message -> {
-                originalMessage = message;
+            acknowledgment.get().thenAccept(message -> {
+                originalMessage.set(message);
                 // Only execute if there is a pending task
-                if (pendingTask != null) {
-                    pendingTask.accept(message);
+                Consumer<InteractionHook> hookConsumer = pendingTask.get();
+                if (hookConsumer != null) {
+                    hookConsumer.accept(message);
                 }
             });
         }
@@ -74,18 +46,49 @@ public interface ReplyCallback {
 
         public <R extends RestAction<Message>> void submitEdit(Function<InteractionHook, R> messageFunction) {
             // Wait for the acknowledgement to complete before editing
-            if (!acknowledgment.isDone()) {
+            if (!acknowledgment.get().isDone()) {
                 // Set the task to execute once the interaction is acknowledged
-                pendingTask = hook -> executingTask = messageFunction.apply(originalMessage).submit();
+                pendingTask.set(hook -> executingTask.set(messageFunction.apply(originalMessage.get()).submit()));
             } else {
                 // Cancel any executing task to avoid spamming Discord's API
-                if (executingTask != null) {
-                    executingTask.cancel(true);
+                if (executingTask.get() != null) {
+                    executingTask.get().cancel(true);
                 }
 
                 // The interaction was already acknowledged, so execute the task now
-                executingTask = messageFunction.apply(originalMessage).submit();
+                executingTask.set(messageFunction.apply(originalMessage.get()).submit());
             }
+        }
+    }
+
+    class DefaultInteractionReplyCallback extends InteractionReplyCallback {
+        private final SlashCommandEvent event;
+
+        public DefaultInteractionReplyCallback(SlashCommandEvent event) {
+            this.event = event;
+        }
+
+        public void sendMessage(MessageCreateData messageData) {
+            if (messageData == null) {
+                acknowledgment.set(acknowledge());
+            } else {
+                acknowledgment.set(event.reply(messageData).setEphemeral(AutoWhitelist.CONFIG.ephemeralReplies).submit());
+            }
+
+            // Execute the pending task after the acknowledgment
+            acknowledgment.get().thenAccept(message -> {
+                originalMessage.set(message);
+                // Only execute if there is a pending task
+                Consumer<InteractionHook> hookConsumer = pendingTask.get();
+                if (hookConsumer != null) {
+                    hookConsumer.accept(message);
+                }
+            });
+        }
+
+        @Override
+        public CompletableFuture<InteractionHook> acknowledge() {
+            return event.deferReply(AutoWhitelist.CONFIG.ephemeralReplies).submit();
         }
     }
 }
