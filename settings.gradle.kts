78360a08e66742e5d60e50f30297050308f72001
LunaFox <awakenedredstone@gmail.com>
2025-07-21 21:33:11 -0300

    chore: 1.21.6 stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 extensions.configure<StonecutterSettings> {
     centralScript = "build.gradle.kts"
     shared {
-        versions("1.20", "1.21", "1.21.2", "1.21.5")
+        versions("1.20", "1.21", "1.21.2", "1.21.5", "1.21.6")
     }
     kotlinController = true
     create(rootProject)
